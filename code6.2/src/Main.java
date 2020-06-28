c76710cf58f52ece7660323c614ac98e6107c337
EllieHan93 <hjhhahahoho@naver.com>
2020-06-28 14:06:17 +0900

    first

---
+++ code6.2/src/Main.java
@@ -0,0 +1,18 @@
+///n개의 원소 중 m개를 고르는 모든 조합을 찾는 알고리
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(sum(10));
+		System.out.println(recursiveSum(10));
+
+	}
+	
+	///n 전체원소수
+	///pricked 지금까지 고른 원소들의 번호
+	public static void Pick (int n, int[] pricked, int toPick)
+	{
+	
+	}
+
+}
